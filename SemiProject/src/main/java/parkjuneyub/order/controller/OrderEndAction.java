ab0f84553b8bdde64fd7a34ba877e8a986c9be25
june <ajsjdlwj0123@gmail.com>
2023-04-20 00:39:04 +0900

    [feat] 주문완료 페이지 클래스 생성

---
+++ SemiProject/src/main/java/parkjuneyub/order/controller/OrderEndAction.java
@@ -0,0 +1,17 @@
+package parkjuneyub.order.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class OrderEndAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		super.setRedirect(false);
+		super.setViewPage("/WEB-INF/order/orderEnd.jsp");
+		
+	}
+
+}
