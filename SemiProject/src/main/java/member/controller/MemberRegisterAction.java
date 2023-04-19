3c66171b1a8d827350009e38c9b1d5b4bd13ba82
user <user@LAPTOP-IN1OIPHK>
2023-04-19 09:33:29 +0900

    회원가입 Action파일입니다

---
+++ SemiProject/src/main/java/member/controller/MemberRegisterAction.java
@@ -0,0 +1,19 @@
+package member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class MemberRegisterAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		
+		//String method = request.getMethod();
+		this.setRedirect(false);
+		this.setViewPage("/WEB-INF/member/memberRegister.jsp");
+		
+	}
+
+}
