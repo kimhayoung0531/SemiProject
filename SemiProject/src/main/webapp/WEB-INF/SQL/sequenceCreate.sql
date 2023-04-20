5f4f0b0b120dc069c59724a99598c5d8e68cc84a
JuneYub <ajsjdlwj0123@gmail.com>
2023-04-20 18:54:12 +0900

    [feat] 새로운 더미 데이터 추가

--- SemiProject/src/main/webapp/WEB-INF/SQL/sequenceCreate.sql
+++ SemiProject/src/main/webapp/WEB-INF/SQL/sequenceCreate.sql
@@ -7,6 +7,15 @@ nominvalue
 nocycle
 nocache;
 
+-- 상품 메인이미지 id 시퀀스
+create sequence product_main_image
+start with 1
+increment by 1
+nomaxvalue
+nominvalue
+nocycle
+nocache;
+
 -- 카테고리 id 시퀀스
 create sequence seq_category_category_num
 start with 1
