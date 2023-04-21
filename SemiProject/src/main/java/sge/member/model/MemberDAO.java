1e8aa621779a26f009fa3468bfdbdef0bf430ecd
user <user@LAPTOP-IN1OIPHK>
2023-04-22 01:56:09 +0900

    DAO 메소드 추가

--- SemiProject/src/main/java/sge/member/model/MemberDAO.java
+++ SemiProject/src/main/java/sge/member/model/MemberDAO.java
@@ -1,7 +1,10 @@
 package sge.member.model;
 
 import java.io.UnsupportedEncodingException;
+
+
 import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 
 import javax.naming.Context;
@@ -46,7 +49,7 @@ public class MemberDAO implements InterMemberDAO {
 				try {
 					Context initContext = new InitialContext();
 					Context envContext = (Context) initContext.lookup("java:/comp/env");
-					ds = (DataSource) envContext.lookup("jdbc/myoracle");
+					ds = (DataSource) envContext.lookup("jdbc/semi_oracle");
 					
 					aes = new AES256(SecretMyKey.KEY);
 				
@@ -61,13 +64,16 @@ public class MemberDAO implements InterMemberDAO {
 	
 	@Override
 	public int registermember(MemberVO member) throws SQLException {
+	
+		System.out.println("~~~~~~ 요기요2 ~~~~~"); 
+	 	
 		int result = 0;
 		
 		try {
 			conn = ds.getConnection();
 			
-			String sql = " insert into tbl_member(userid, pwd, name, email, mobile, postcode, address, detailaddress, extraaddress, gender, birthday) "
-					   + " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
+			String sql = " insert into tbl_member(user_id, pwd, user_name, email, mobile, telephone, post_code, address, detailAddress, extraAddress, gender, birthday) "
+					   + " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ";
 			pstmt = conn.prepareStatement(sql);
 			
 		
@@ -76,23 +82,54 @@ public class MemberDAO implements InterMemberDAO {
 			pstmt.setString(3, member.getUser_name());
 			pstmt.setString(4, aes.encrypt(member.getEmail())); // 이메일을 AES256알고리즘으로 양방향 암호화 시킨다
 			pstmt.setString(5, aes.encrypt(member.getMobile())); // 휴대폰번호을 AES256알고리즘으로 양방향 암호화 시킨다
-			pstmt.setString(6, member.getPost_code());
-			pstmt.setString(7, member.getAddress());
-			pstmt.setString(8, member.getDetailAddress());
-			pstmt.setString(9, member.getExtraAddress());
-			pstmt.setString(10, member.getGender());
-			pstmt.setString(11, member.getBirthday());
+			pstmt.setString(6, aes.encrypt(member.getTelephone())); // 휴대폰번호을 AES256알고리즘으로 양방향 암호화 시킨다.
+			pstmt.setString(7, member.getPost_code());
+			pstmt.setString(8, member.getAddress());
+			pstmt.setString(9, member.getDetailAddress());
+			pstmt.setString(10, member.getExtraAddress());
+			pstmt.setString(11, member.getGender());
+			pstmt.setString(12, member.getBirthday());
 			
 			result = pstmt.executeUpdate();
 			
 			
-		} catch(GeneralSecurityException | UnsupportedEncodingException e) {
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
-		}finally {	
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GeneralSecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {	
 			close();
 		}
 		return result;
-	}
+	}// end of public int registermember(MemberVO member)
+	
+	// 아이디가 중복된건지 아닌지 확인해주는 메소드
+	@Override
+	public Boolean idDoubleCheck(String user_id) throws SQLException {
+		 Boolean idExist = false;
+		 try {
+			conn = ds.getConnection();
+			
+			String sql = " select * " +
+					     " from tbl_member " +
+						 " where user_id = ? ";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user_id);
+			
+			rs = pstmt.executeQuery();
+			idExist = rs.next();
+			
+		} finally {
+			close();
+		}
+	
+		return idExist;
+	}// end of idDoubleCheck
 
 }
