f6c1a7eecf9b8b6da0a1e842ecd92907645792e5
realsoly <jk120926@gmail.com>
2023-04-22 16:17:51 +0900

    [feat] cartAction.java 수정

--- SemiProject/src/main/java/jinsol/cart/controller/CartAction.java
+++ SemiProject/src/main/java/jinsol/cart/controller/CartAction.java
@@ -1,20 +1,24 @@
 package jinsol.cart.controller;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
-
 public class CartAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 
-		super.setRedirect(false);
-		super.setViewPage("/WEB-INF/kjs_cart/cart.jsp");
+		    String itemCnt = request.getParameter("itemCnt");
 		
+		    request.setAttribute("itemCnt", itemCnt);
+		    
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/kjs_cart/cart.jsp");
+			
 		
-	}
+	} //end of public void execute ---------------------------------------
 
 	
 }
