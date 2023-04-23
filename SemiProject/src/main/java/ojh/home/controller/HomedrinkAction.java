e26b88bf3a1dc83a0ebd3c2b1d7d4868b947891e
sist <sist@DESKTOP-240M8SR>
2023-04-24 00:43:18 +0900

    [feat] 전체 카테고리 음료수 java 추가

---
+++ SemiProject/src/main/java/ojh/home/controller/HomedrinkAction.java
@@ -0,0 +1,18 @@
+package ojh.home.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class HomedrinkAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		super.setRedirect(false);
+		super.setViewPage("/WEB-INF/ojh_home/homedrink.jsp");
+		
+	}
+
+}
