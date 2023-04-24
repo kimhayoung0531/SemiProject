7b701a163fa1cf1ad374ab7f13d16e0701f2a0b0
user <user@LAPTOP-IN1OIPHK>
2023-04-24 17:50:53 +0900

    [feat] 본인인증 기능 파일 생성

---
+++ SemiProject/src/main/java/sge/member/controller/PwdCertificateNumberAction.java
@@ -0,0 +1,16 @@
+package sge.member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class PwdCertificateNumberAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		super.setRedirect(false);
+		super.setViewPage("/WEB-INF/sge_login/pwd_certificateNum.jsp");
+	}
+
+}
