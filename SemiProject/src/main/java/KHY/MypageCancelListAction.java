f52d70b5a7ced29d2196470a29fbcd7d893b81dc
kimhayoung <kim05hy31@naver.com>
2023-04-24 16:17:18 +0900

    김하영 이미지 추가 및 기간 조회 기능 수정

--- SemiProject/src/main/java/KHY/MypageCancelListAction.java
+++ SemiProject/src/main/java/KHY/MypageCancelListAction.java
@@ -5,12 +5,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
 
-public class MypageController extends AbstractController {
+public class MypageCancelListAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		super.setViewPage("/WEB-INF/KHY/mypage_main.jsp");
+		super.setViewPage("/WEB-INF/KHY/mypage_cancel_list.jsp");
 		
 	}
 
