a606a9c01b265cb9f0d42e4cffe5b04b4212a6de
JuneYub <ajsjdlwj0123@gmail.com>
2023-04-25 16:01:42 +0900

    [fix] 충돌 해결을 위해서 덮어씌웠습니다.

--- SemiProject/src/main/java/jinsol/cart/model/CartVO.java
+++ SemiProject/src/main/java/jinsol/cart/model/CartVO.java
@@ -5,54 +5,6 @@ import sge.member.model.MemberVO;
 
 public class CartVO implements InterCartDAO{
 
-	public long getCart_num() {
-		return cart_num;
-	}
-
-	public void setCart_num(long cart_num) {
-		this.cart_num = cart_num;
-	}
-
-	public long getProduct_num() {
-		return product_num;
-	}
-
-	public void setProduct_num(long product_num) {
-		this.product_num = product_num;
-	}
-
-	public long getProduct_count() {
-		return product_count;
-	}
-
-	public void setProduct_count(long product_count) {
-		this.product_count = product_count;
-	}
-
-	public String getCart_date() {
-		return cart_date;
-	}
-
-	public void setCart_date(String cart_date) {
-		this.cart_date = cart_date;
-	}
-
-	public MemberVO getMvo() {
-		return mvo;
-	}
-
-	public void setMvo(MemberVO mvo) {
-		this.mvo = mvo;
-	}
-
-	public ProductVO getPvo() {
-		return pvo;
-	}
-
-	public void setPvo(ProductVO pvo) {
-		this.pvo = pvo;
-	}
-
 	private long cart_num;
 	private long product_num;
 	private long product_count;
@@ -60,18 +12,50 @@ public class CartVO implements InterCartDAO{
 	
 	// select용
 	private MemberVO mvo;
-	private ProductVO pvo;
-	 
+	private ProductVO pvo; 
 	public CartVO() {}
-
-
 	
-	
-	////////////////////////////////////////////
+	///////////////////////////////////////////////////
 	// Getter Setter
 	
-	
-	
+	public long getCart_num() {
+		return cart_num;
+	}
+	public void setCart_num(long cart_num) {
+		this.cart_num = cart_num;
+	}
+	public long getProduct_num() {
+		return product_num;
+	}
+	public void setProduct_num(long product_num) {
+		this.product_num = product_num;
+	}
+	public long getProduct_count() {
+		return product_count;
+	}
+	public void setProduct_count(long product_count) {
+		this.product_count = product_count;
+	}
+	public String getCart_date() {
+		return cart_date;
+	}
+	public void setCart_date(String cart_date) {
+		this.cart_date = cart_date;
+	}
+	public MemberVO getMvo() {
+		return mvo;
+	}
+	public void setMvo(MemberVO mvo) {
+		this.mvo = mvo;
+	}
+	public ProductVO getPvo() {
+		return pvo;
+	}
+	public void setPvo(ProductVO pvo) {
+		this.pvo = pvo;
+	}
+
+
 	
 	
 }
