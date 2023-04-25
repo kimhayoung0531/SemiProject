7ef60d0c5d0ba722456aefb9a57101ed5ae53a39
realsoly <jk120926@gmail.com>
2023-04-25 19:27:12 +0900

    Merge branch 'main' into KJS

--- SemiProject/src/main/java/jinsol/cart/model/CartDAO.java
+++ SemiProject/src/main/java/jinsol/cart/model/CartDAO.java
@@ -48,7 +48,7 @@ public class CartDAO implements InterCartDAO {
 
 	
 	
-	// 장바구니 담기 
+	// 장바구니 담기 ~
     // 장바구니 테이블(tbl_cart)에 해당 제품을 담아야 한다.
     // 장바구니 테이블에 해당 제품이 존재하지 않는 경우에는 tbl_cart 테이블에 insert 를 해야하고, 
     // 장바구니 테이블에 해당 제품이 존재하는 경우에는 또 그 제품을 추가해서 장바구니 담기를 한다라면 tbl_cart 테이블에 update 를 해야한다. 
