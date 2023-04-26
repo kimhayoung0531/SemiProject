fa4a202d642e0c9c13aafbd353c21915d0f44ba1
user <user@LAPTOP-IN1OIPHK>
2023-04-27 00:48:27 +0900

    [feat] 본인인증 기능

--- SemiProject/src/main/java/sge/member/controller/PwdCertificateNumberAction.java
+++ SemiProject/src/main/java/sge/member/controller/PwdCertificateNumberAction.java
@@ -1,5 +1,7 @@
 package sge.member.controller;
 
+
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,34 +13,55 @@ public class PwdCertificateNumberAction extends AbstractController {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		String userCertificationCode = request.getParameter("userCertificationCode");
-		String user_id = request.getParameter("user_id");
-		
-		//세션 불러오기 
+		String method = request.getMethod();
 		HttpSession session = request.getSession();
-		String certificationCode = (String)session.getAttribute("certificationCode");	
-	
-		String message = "";
-		String loc = "";
 		
-		if( certificationCode.equals(userCertificationCode) ) {
-			message = "인증 성공 되었습니다";
-			loc = request.getContextPath()+"/pwdChange.ban.up?user_id="+user_id;
-		}
-		/*
-		 * else { message = "발급된 인증 코드가 아닙니다.인증코드를 다시 발급받으세요!!"; loc =
-		 * request.getContextPath()+"/findPwd.ban"; }
-		 */
-		request.setAttribute("message", message);
-		request.setAttribute("loc", loc);
-		
-		super.setRedirect(false);
-		super.setViewPage("/WEB-INF/msg.jsp");
-		
-		//!! 중요  !!
-		// 세션에 저장된 이증코드 삭제하기 
-		session.removeAttribute("certificationCode");
+		if("post".equalsIgnoreCase(method)) {
+			String userCertificationCode = request.getParameter("userCertificationCode");
+			//String user_id = request.getAttribute("user_id");
+			
+			System.out.println(userCertificationCode);
+			//System.out.println(user_id);
 
+			//세션 불러오기 
+			
+			String certificationCode = (String)session.getAttribute("certificationCode");
+			String email = (String)session.getAttribute("email");
+			
+			
+			System.out.println(certificationCode);
+		
+			String message = "";
+			String loc = "";
+			
+			if( certificationCode.equals(userCertificationCode) ) {
+				message = "인증 성공 되었습니다";
+				loc = request.getContextPath()+"/pwdChange.ban?email=" + email;
+			}
+			
+			
+		    else { 
+		    	message = "발급된 인증 코드가 아닙니다.인증코드를 다시 발급받으세요!!"; 
+		        loc = request.getContextPath()+"/findPwd.ban";
+		    }
+			 
+			request.setAttribute("message", message);
+			request.setAttribute("loc", loc);
+			
+			super.setRedirect(false);
+			super.setViewPage("/WEB-INF/msg.jsp");
+			
+		}
+		else {
+			 super.setRedirect(false);
+			 super.setViewPage("/WEB-INF/sge_login/pwd_certificateNum.jsp");
+		}
+	
+
+		//!! 중요  !!
+		// 세션에 저장된 인증코드 삭제하기 
+		// session.removeAttribute("certificationCode");
+		
 		
 	}
 
