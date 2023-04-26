3634a85df79f13e841e59034e1bd14e313e5c4ec
user <user@LAPTOP-IN1OIPHK>
2023-04-26 18:42:31 +0900

    [feat] 비밀번호 인증

--- SemiProject/src/main/java/sge/member/controller/PwdCertificateNumberAction.java
+++ SemiProject/src/main/java/sge/member/controller/PwdCertificateNumberAction.java
@@ -2,6 +2,7 @@ package sge.member.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import common.controller.AbstractController;
 
@@ -9,8 +10,36 @@ public class PwdCertificateNumberAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String userCertificationCode = request.getParameter("userCertificationCode");
+		String user_id = request.getParameter("user_id");
+		
+		//세션 불러오기 
+		HttpSession session = request.getSession();
+		String certificationCode = (String)session.getAttribute("certificationCode");	
+	
+		String message = "";
+		String loc = "";
+		
+		if( certificationCode.equals(userCertificationCode) ) {
+			message = "인증 성공 되었습니다";
+			loc = request.getContextPath()+"/pwdChange.ban.up?user_id="+user_id;
+		}
+		/*
+		 * else { message = "발급된 인증 코드가 아닙니다.인증코드를 다시 발급받으세요!!"; loc =
+		 * request.getContextPath()+"/findPwd.ban"; }
+		 */
+		request.setAttribute("message", message);
+		request.setAttribute("loc", loc);
+		
 		super.setRedirect(false);
-		super.setViewPage("/WEB-INF/sge_login/pwd_certificateNum.jsp");
+		super.setViewPage("/WEB-INF/msg.jsp");
+		
+		//!! 중요  !!
+		// 세션에 저장된 이증코드 삭제하기 
+		session.removeAttribute("certificationCode");
+
+		
 	}
 
 }
