832b4960ef30665e452a1727d0ff2e9629c4dfa2
user <user@LAPTOP-IN1OIPHK>
2023-04-29 18:15:08 +0900

    [feat] 상품등록페이지 패이지 생성

---
+++ SemiProject/src/main/java/sge/admin/controller/ProductRegisterAction.java
@@ -0,0 +1,17 @@
+package sge.admin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.controller.AbstractController;
+
+public class ProductRegisterAction extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	
+		super.setRedirect(false);
+		super.setViewPage("/WEB-INF/sge_admin/productRegister.jsp");
+	}
+
+}
