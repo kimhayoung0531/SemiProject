e64999ef6e37fe2578a46c32b79661d1a9f916e0
JuneYub <ajsjdlwj0123@gmail.com>
2023-05-01 02:11:19 +0900

    [fix] 구매후기 작성을 위한 상품 상세페이지 버그 수정

--- SemiProject/src/main/java/parkjuneyub/product/controller/ProductDetailAction.java
+++ SemiProject/src/main/java/parkjuneyub/product/controller/ProductDetailAction.java
@@ -1,11 +1,15 @@
 package parkjuneyub.product.controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
+import parkjuneyub.board.model.BoardDAO;
+import parkjuneyub.board.model.InterBoardDAO;
+import parkjuneyub.board.model.ReviewVO;
 import parkjuneyub.product.model.InterProductDAO;
 import parkjuneyub.product.model.ProductDAO;
 import parkjuneyub.product.model.ProductVO;
@@ -50,11 +54,13 @@ public class ProductDetailAction extends AbstractController {
 			
 			
 			ProductVO pvo = new ProductVO();
-			
 			InterProductDAO pdao = new ProductDAO();
-			
 			pvo = pdao.showItemInfo(product_num);
 			
+			InterBoardDAO bdao = new BoardDAO();
+			List<ReviewVO> reviewList = bdao.getReivewList(product_num);
+			
+			request.setAttribute("reviewList", reviewList);
 			request.setAttribute("pvo", pvo);
 			super.setRedirect(false);
 			super.setViewPage("/WEB-INF/pjy_product/productDetail.jsp");
