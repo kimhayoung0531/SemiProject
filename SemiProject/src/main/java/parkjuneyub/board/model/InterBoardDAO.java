e89301122d0633c0291b80c2035762bc13b864b3
JuneYub <ajsjdlwj0123@gmail.com>
2023-05-02 08:52:08 +0900

    [feat] 페이징 처리를 위한 메소드 기능 구현

--- SemiProject/src/main/java/parkjuneyub/board/model/InterBoardDAO.java
+++ SemiProject/src/main/java/parkjuneyub/board/model/InterBoardDAO.java
@@ -10,12 +10,15 @@ import parkjuenyub.order.model.OrderVO;
 public interface InterBoardDAO {
 	
 	// 상품번호를 가지고 관련 리뷰 글을 가져오는 메소드
-	List<ReviewVO> getReivewList(String product_num) throws SQLException;
+	List<ReviewVO> getReviewList(Map<String, String> paraMap) throws SQLException;
 
 	// 리뷰를 작성하는 메소드
 	int insertReview(Map<String, Object> map) throws SQLException;
 	
 	// 아이디랑 상품번호로 주문번호를 조회해오는 메소드
 	List<OrderVO> getOrderDeatailList(String user_id, String product_num) throws SQLException;
+
+	//한 상품의 전체 리뷰를 가져온다.
+	int getTotalPage(Map<String, String> paraMap) throws SQLException;
 	
 }
