eedfdf17df0fe2b6066b552c2ca941ca06212934
sist <sist@DESKTOP-240M8SR>
2023-05-03 15:52:28 +0900

    [feat] 베스트 상품 작업 중

--- SemiProject/src/main/java/ojh/home/controller/HomebestAction.java
+++ SemiProject/src/main/java/ojh/home/controller/HomebestAction.java
@@ -1,14 +1,28 @@
 package ojh.home.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
+import ojh.home.model.*;
+import parkjuneyub.product.model.ProductVO;
 
 public class HomebestAction extends AbstractController {
-
+	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String category_num = "1";
+		
+		InterHomeDAO hdao = new HomeDAO();
+		
+		List<ProductVO> pvoList = hdao.getProductInfo(category_num);
+		
+		request.setAttribute("pvoList", pvoList);
 		
 		super.setRedirect(false);
 		super.setViewPage("/WEB-INF/ojh_home/homebest.jsp");
@@ -16,3 +30,5 @@ public class HomebestAction extends AbstractController {
 	}
 
 }
+
+
