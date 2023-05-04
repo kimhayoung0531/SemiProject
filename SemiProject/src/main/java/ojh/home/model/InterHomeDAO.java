3cb2e8ebabc2dc87007c045e97c734f5061463d2
sist <sist@DESKTOP-240M8SR>
2023-05-04 09:00:07 +0900

    [feat] 메소드 추가

--- SemiProject/src/main/java/ojh/home/model/InterHomeDAO.java
+++ SemiProject/src/main/java/ojh/home/model/InterHomeDAO.java
@@ -11,11 +11,19 @@ public interface InterHomeDAO {
 
 	// 카테고리 상품 가져오기
 	List<ProductVO> getProductInfo(String category_num) throws SQLException;
-	// 베스트 상품 가져오기
+	
+	// 카테고리 베스트 상품 데이터 출력
+	List<ProductVO> getCatBestItemBysalecount() throws SQLException;
+	
+	// 메인페이지 베스트 이벤트 데이터 출력
 	List<ProductVO> getBestItemBysalecount() throws SQLException;
+
+	// 메인페이지 신상품 이벤트 데이터 출력
+	List<ProductVO> getNewProduct() throws SQLException;
 	
 	
-	/* List<ProductVO> getBestItemBysalecount() throws SQLException; */
+	
+	
 	
 	
 	
